5de984711157aee4a96d7c9c0f841e4cfb984818
AlxGrshn <113042128+AlxGrshn@users.noreply.github.com>
2022-11-21 19:05:46 +0300

    release base app logic

---
+++ TaskListApp/App/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  TaskListApp
+//
+//  Created by Alexander Grishin on 21.11.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: TaskListViewController())
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        StorageManager.shared.saveContext()
+    }
+}
+
---
+++ TaskListApp/Services/StorageManager.swift
@@ -0,0 +1,48 @@
+//
+//  StorageManager.swift
+//  TaskListApp
+//
+//  Created by Alexander Grishin on 21.11.2022.
+//
+
+import Foundation
+import CoreData
+
+class StorageManager {
+    static let shared = StorageManager()
+    
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TaskList")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func fetchData(_ completion: ([Task]) -> Void) {
+        let fetchRequest = Task.fetchRequest()
+        do {
+            let task = try persistentContainer.viewContext.fetch(fetchRequest)
+            completion(task)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+   private init() {}
+}
+
